0abd6b69446ccc53ecb199aff6f1e012725c09e5
Kristof Raes <kristof.raes@arteveldehs.be>
2023-04-27 11:17:31 +0200

    Update Program.cs

--- 2223-lessons/Lesson07/Excercise01/Program.cs
+++ 2223-lessons/Lesson07/Excercise01/Program.cs
@@ -1,4 +1,4 @@
-﻿internal class Program
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -8,8 +8,65 @@
         tafel1.Test();
         Tafel tafel2 = new Tafel("grote tafel", 10);
         tafel2.Test();
+
+        Computer c1 = new Computer();
+        c1.pcType = "Laptop";
+        //...
+        Laptop lpt1 = new Laptop("13 inch","HP Probook");
+        Console.WriteLine("Type: " + lpt1.pcType + "; Display: " + lpt1.Display);
+    
     }
 }
+
+
+public class Computer
+{
+    public string pcType;
+
+    public Computer()
+    {
+    }
+
+    public Computer(string pc)
+    {
+        this.pcType = pc;
+    }
+
+
+}
+
+public class Laptop : Computer
+{
+    private string display;
+
+    public string Display
+    {
+         get { return display;}
+        set { display = value;}
+    }
+
+    public Laptop()
+    {}
+    public Laptop(string displ, string pc) : base (pc)
+    {
+        this.display = displ;
+        //base.pcType = 
+    }
+}
+
+/*
+public class Desktop : Computer
+{
+    public Desktop()
+    {}
+
+    public Desktop()
+    {
+
+    }
+
+}
+*/
 
 public class Tafel
 {
