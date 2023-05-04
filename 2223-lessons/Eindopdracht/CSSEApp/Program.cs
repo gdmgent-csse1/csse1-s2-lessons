0b094681a87d06f626590465dfdeaff4a825d883
Kristof Raes <kristof.raes@arteveldehs.be>
2023-05-04 11:57:53 +0200

    Add files via upload

---
+++ 2223-lessons/Eindopdracht/CSSEApp/Program.cs
@@ -0,0 +1,8 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        CSSEClass.MathOperations.Test();
+    }
+}
---
+++ 2223-lessons/Eindopdracht/CSSEClass/MathOperations.cs
@@ -0,0 +1,9 @@
+
+namespace CSSEClass;
+public class MathOperations
+{
+    public static void Test()
+    {
+        Console.WriteLine("Test!");
+    }
+}
